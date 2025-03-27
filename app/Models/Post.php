f70215c2d1cd2f5d636f66350768b124b09ffcb6
rgulbis <rgubis41@gmail.com>
2025-03-27 09:32:23 +0200

    plz push

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
     protected $fillable = ["content", "category_id"];
 
     public function comments(): HasMany
