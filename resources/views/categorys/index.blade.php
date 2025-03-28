4f307731f00667a74740b5900932570b5f9b8002
rgulbis <rgubis41@gmail.com>
2025-03-28 12:23:34 +0200

    + css

--- resources/views/categorys/index.blade.php
+++ resources/views/categorys/index.blade.php
@@ -1,9 +1,12 @@
 <x-layout>
-    <x-slot:title>Categorys</x-slot:title>
-    <h1>Categorys</h1>
-    <ul>
-        @foreach ($categorys as $category)
-            <li><a href="/categorys/{{ $category->id }}">{{ $category->category_name }}</a></li>
-        @endforeach
-    </ul>
+    <x-slot:title>Categories</x-slot:title>
+    <div class="categories-all">
+        <h1>Categories</h1>
+        <hr>
+        <ul>
+            @foreach ($categorys as $category)
+                <li><a href="/categorys/{{ $category->id }}">{{ $category->category_name }}</a></li>
+            @endforeach
+        </ul>
+    </div>
 </x-layout>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,23 +1,20 @@
 <x-layout>
     <x-slot:title>Create</x-slot:title>
-    <h1>Create</h1>  
-    <form method="POST" action="/posts">
-        @csrf
-        <textarea name="content" placeholder="New post.."></textarea>
-        <select name="category_id">
-            <option value="">None</option>
-            @foreach ($categorys as $category)
-                <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-            @endforeach
-        </select>
-        @error('content')
-            <p>{{ $message }}</p>
-        @enderror
-        @error('category_id')
-            <p>{{ $message }}</p>
-        @enderror
-        <button>Submit</button>
-    </form>
+    <div class="create-post">
+        <h1>Create</h1>  
+        <hr>
+        <form method="POST" action="/posts">
+            @csrf
+            <textarea name="content" placeholder="New post.."></textarea>
+            <select name="category_id">
+                <option value="">None</option>
+                @foreach ($categorys as $category)
+                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                @endforeach
+            </select>
+            <button id="pointer">Submit</button>
+        </form>
+    </div>
     <x-slot:categories>
     </x-slot:categories>
 </x-layout>
