5a84fd47e32e1fe3c28dba8c73eadc477f2624b1
rgulbis <rgubis41@gmail.com>
2025-04-03 23:08:59 +0300

    + ?

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,7 +37,7 @@
             <li>
                 <div class="post">
                     <p>{{ $post->content }}</p>
-                    <div class="options-wrap" id="{{ !(Auth::user()->id == $post->user_id) ? "hidden" : "" }}">
+                    <div class="options-wrap" id="{{ !(Auth::user()?->id == $post->user_id) ? "hidden" : "" }}">
                         <svg class="options-dots" id="pointer" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>
                         <div class="options">
                             <a href="/posts/{{ $post->id }}/edit">Edit</a>
@@ -64,7 +64,7 @@
                             <h4 class="comment-name">{{ $comment->name }}</h4>
                             <h4 class="comment-date">{{ $comment->created_at->timezone('Europe/Riga')->format('d.m.Y H:i:s') }}</h4>
                             <p class="comment-comment">{{ $comment->comment }}</p>
-                            <div class="options-wrap" id="{{ !(Auth::user()->id == $comment->user_id) ? "hidden" : "" }}">
+                            <div class="options-wrap" id="{{ !(Auth::user()?->id == $comment->user_id) ? "hidden" : "" }}">
                                 <svg class="options-dots" id="pointer" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>
                                 <div class="options">
                                     <a href="/comments/{{ $comment->id }}/edit">Edit</a>
